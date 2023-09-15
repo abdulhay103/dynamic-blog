3ed20dd827ba28df126972653df51598201ff87e
abdulhay103 <abdulhay.me@gmail.com>
2023-09-15 21:13:44 +0600

    working process

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { signIn } from "next-auth/react";
+// import { signIn } from "next-auth/react";
 import { useState } from "react";
 import Link from "next/link";
 
@@ -79,7 +79,7 @@ export default function Login() {
           </button>
         </div>
       </form>
-      <div className="flex gap-5">
+      {/* <div className="flex gap-5">
         <button
           className="py-2 px-5 bg-sky-300 text-white rounded"
           onClick={() => {
@@ -96,7 +96,7 @@ export default function Login() {
         >
           Google
         </button>
-      </div>
+      </div> */}
       <div className="text-center py-3">
         <p>
           I'm not registered yet!
