b5b8dc0e50c117f3ffda3cf4653f51d7a04ca0ed
abdulhay103 <abdulhay.me@gmail.com>
2023-09-18 11:39:57 +0600

    authentication done

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,11 @@
+"use client";
+import { useSession } from "next-auth/react";
+
 export default function Dashboard() {
+  const { data, status } = useSession();
   return (
     <div>
-      <h2>Dashboard Page</h2>
+      <h2>Dashboard Page for {data.user.email}</h2>
     </div>
   );
 }
